6f1e6560e50f6f94657f7ad59af43df30382fd1a
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 02:41:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('menuBtn')?.addEventListener('click',()=>document.getElementById('nav')?.classList.toggle('open'));
+setTimeout(()=>document.getElementById('intro')?.remove(),3400);
