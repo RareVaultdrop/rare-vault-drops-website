0bbd6d0b3084037ea6e3bb2ff414fe56a25eccbd
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 00:21:55 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const cards=document.querySelectorAll('.cards article,.panel,.insta-grid div');
-cards.forEach(card=>{
-  card.addEventListener('mousemove',e=>{
-    const r=card.getBoundingClientRect();
-    card.style.transform=`perspective(900px) rotateX(${-(e.clientY-r.top-r.height/2)/35}deg) rotateY(${(e.clientX-r.left-r.width/2)/35}deg)`;
-  });
-  card.addEventListener('mouseleave',()=>card.style.transform='perspective(900px) rotateX(0) rotateY(0)');
-});
