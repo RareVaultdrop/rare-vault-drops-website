6180b66002d1bde2e683a8772d90cab87e7adfb5
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 03:35:37 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('menuBtn')?.addEventListener('click',()=>document.getElementById('navMenu')?.classList.toggle('open'));
-setTimeout(()=>document.getElementById('intro')?.remove(),3800);
-const obs=new IntersectionObserver((entries)=>entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')}),{threshold:.14});
-document.querySelectorAll('.reveal').forEach(el=>obs.observe(el));
