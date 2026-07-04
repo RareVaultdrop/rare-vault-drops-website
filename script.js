a0e648389c059ea000858a271e8e1bd864a046e4
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 03:03:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menuBtn')?.addEventListener('click',()=>document.getElementById('nav')?.classList.toggle('open'));
-setTimeout(()=>document.getElementById('intro')?.remove(),3400);
-const obs = new IntersectionObserver((entries)=>entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')}), {threshold:.15});
-document.querySelectorAll('.reveal').forEach(el=>obs.observe(el));
