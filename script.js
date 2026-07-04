ad3d80b07123965c157b40178adf1082f4350096
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 02:50:58 -0500

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('menuBtn')?.addEventListener('click',()=>document.getElementById('nav')?.classList.toggle('open'));
-setTimeout(()=>document.getElementById('intro')?.remove(),3400);
