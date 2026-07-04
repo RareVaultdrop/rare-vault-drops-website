3587b19e31039d3943a0d95596d1a8e7f501467f
RareVaultdrop <rvd@rarevaultdrop.com>
2026-07-04 03:01:05 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburger')?.addEventListener('click',()=>document.getElementById('nav')?.classList.toggle('open'));
-setTimeout(()=>document.getElementById('loader')?.remove(),3400);
-const obs = new IntersectionObserver((entries)=>entries.forEach(e=>{ if(e.isIntersecting) e.target.classList.add('show') }), {threshold:.15});
-document.querySelectorAll('.reveal').forEach(el=>obs.observe(el));
